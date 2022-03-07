d04f6346db73671811c2c452a1cb920beacf9669
SurabhiBorase <surabhiborase44@gmail.com>
2022-03-07 23:06:44 +0530

    MODIFIED: Employee Part-Time or Full-Time

--- src/com/infogalaxy/EmployeeWageBuilder.java
+++ src/com/infogalaxy/EmployeeWageBuilder.java
@@ -6,7 +6,8 @@ public class EmployeeWageBuilder {
     public static void main(String[] args) {
         //UC-2 Calculate the Daily Wage
         final int WAGE_PER_HR = 20;
-        final int FULL_DAY_HR = 8;
+        final int FULL_TIME = 8;
+        final int PART_TIME = 4;
         int total_Wage = 0;
         //UC-1 Employee Attendance
         Random random = new Random();
@@ -16,7 +17,15 @@ public class EmployeeWageBuilder {
             System.out.println("Employee is Absent");
         } else {
             System.out.println("Employee is Present");
-            total_Wage = WAGE_PER_HR * FULL_DAY_HR;
+            //UC-3 Employee Part time or Full Time
+            int empType = random.nextInt() % 2;
+            if(empType == 0){
+                System.out.println("Full Time");
+                total_Wage = WAGE_PER_HR * FULL_TIME;
+            } else {
+                System.out.println("Part Time");
+                 total_Wage = WAGE_PER_HR * PART_TIME;
+            }
         }
         System.out.println("Total Wage ="+total_Wage);
 
