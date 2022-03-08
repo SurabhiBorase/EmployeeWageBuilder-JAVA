0c7362da39fee1b319a8a5efff407a5263f867a3
SurabhiBorase <surabhiborase44@gmail.com>
2022-03-08 10:56:36 +0530

    MODIFIED: Calculate the wage for 20-Days

--- src/com/infogalaxy/EmployeeWageBuilder.java
+++ src/com/infogalaxy/EmployeeWageBuilder.java
@@ -11,30 +11,30 @@ public class EmployeeWageBuilder {
         int total_Wage = 0;
         //UC-1 Employee Attendance
         Random random = new Random();
-        int empAttendance = random.nextInt() % 2;         //Generate the random NO
-        System.out.println(empAttendance);
-        if (empAttendance == 0) {
-            System.out.println("Employee is Absent");
-        } else {
-            System.out.println("Employee is Present");
-            //UC-4 Employee Part time or Full Time using SWITCH-CASE
-            int empType = random.nextInt() % 2;
-            switch (empType) {
-                case 0:
-                    System.out.println("Full Time");
-                    total_Wage = WAGE_PER_HR * FULL_TIME;
-                    break;
-                case 1:
-                    System.out.println("Part Time");
-                    total_Wage = WAGE_PER_HR * PART_TIME;
-                    break;
-                default:
-                    System.out.println("Invalid No");
-                    break;
+        //UC-5 Calculate Wage for 20 Days
+        for(int i = 1; i < 20; i++) {
+            int empAttendance = random.nextInt() % 2;         //Generate the random NO
+            System.out.println(empAttendance);
+            if (empAttendance == 0) {
+                System.out.println("Employee is Absent");
+            } else {
+                System.out.println("Employee is Present");
+                //UC-4 Employee Part time or Full Time using SWITCH-CASE
+                int empType = random.nextInt() % 2;
+                switch (empType) {
+                    case 0:
+                        System.out.println("Full Time");
+                        total_Wage = WAGE_PER_HR * FULL_TIME;
+                        break;
+                    case 1:
+                    case -1:
+                        System.out.println("Part Time");
+                        total_Wage = WAGE_PER_HR * PART_TIME;
+                        break;
+                }
+
             }
-
+            System.out.println("Total Wage =" + total_Wage);
         }
-        System.out.println("Total Wage =" + total_Wage);
-
     }
 }
