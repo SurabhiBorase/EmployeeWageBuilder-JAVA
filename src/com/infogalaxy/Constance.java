82a7214e0fe67f7a599057546dff8431b2952145
SurabhiBorase <surabhiborase44@gmail.com>
2022-03-10 11:32:09 +0530

    MODIFIED: Use Class Method and Class Variable for Employee-Wage

---
+++ src/com/infogalaxy/Constance.java
@@ -0,0 +1,12 @@
+package com.infogalaxy;
+
+public class Constance {
+    final static int WAGE_PER_HR = 20;
+    final static int FULL_TIME = 8;
+    final static int PART_TIME = 4;
+    final static int WORKING_DAYS = 20;
+    final static int IS_ABSENT = 0;
+    final static int IS_FULL_TIME = 0;
+    final static int IS_PART_TIME = 1;
+    final static int TOTAL_WORKING_HR = 100;
+}
--- src/com/infogalaxy/EmployeeWageBuilder.java
+++ src/com/infogalaxy/EmployeeWageBuilder.java
@@ -3,51 +3,49 @@ package com.infogalaxy;
 import java.util.Random;
 
 public class EmployeeWageBuilder {
-    public static void main(String[] args) {
-        //UC-2 Calculate the Daily Wage
-        final int WAGE_PER_HR = 20;
-        final int FULL_TIME = 8;
-        final int PART_TIME = 4;
-        final int WORKING_DAYS = 20;
-        final int IS_ABSENT = 0;
-        final int IS_FULL_TIME = 0;
-        final int IS_PART_TIME = 1;
-        final int TOTAL_WORKING_HR = 100;
-        int total_Wage = 0;
-        int total_hrs = 0;
-        int working_days = 0;
-        //UC-1 Employee Attendance
-        Random random = new Random();
-        //UC-6 Calculate Wage for 20 Days or total Working Hours upto 100
-        while(working_days <= WORKING_DAYS && total_hrs <= TOTAL_WORKING_HR) {
+    int total_Wage = 0;
+    int total_hrs = 0;
+    int working_days = 0;
+    int sum_totalWage = 0;
+    Random random = new Random();
+
+    public void empWageCalculation() {
+        while (working_days < Constance.WORKING_DAYS && total_hrs <= Constance.TOTAL_WORKING_HR) {
             working_days++;
+            System.out.println("Day :" + working_days);
             int empAttendance = random.nextInt() % 2;         //Generate the random NO
             System.out.println(empAttendance);
-            if (empAttendance == IS_ABSENT) {
+            if (empAttendance == Constance.IS_ABSENT) {
                 System.out.println("Employee is Absent");
             } else {
                 System.out.println("Employee is Present");
-                //UC-4 Employee Part time or Full Time using SWITCH-CASE
                 int empType = random.nextInt() % 2;
-                if(empType < 0) {
+                if (empType < 0) {
                     empType = -empType;
                 }
                 switch (empType) {
-                    case IS_FULL_TIME:
+                    case Constance.IS_FULL_TIME:
                         System.out.println("Full Time");
-                        total_Wage = WAGE_PER_HR * FULL_TIME;
-                        total_hrs = total_hrs + FULL_TIME;
+                        total_Wage = Constance.WAGE_PER_HR * Constance.FULL_TIME;
+                        total_hrs = total_hrs + Constance.FULL_TIME;
                         break;
-                    case IS_PART_TIME:
+                    case Constance.IS_PART_TIME:
                         System.out.println("Part Time");
-                        total_Wage = WAGE_PER_HR * PART_TIME;
-                        total_hrs = total_hrs + PART_TIME;
+                        total_Wage = Constance.WAGE_PER_HR * Constance.PART_TIME;
+                        total_hrs = total_hrs + Constance.PART_TIME;
                         break;
                 }
-
             }
-            System.out.println("Total Wage =" + total_Wage);
-            System.out.println("Total Hours =" + total_hrs);
+            sum_totalWage = total_Wage + sum_totalWage;
         }
+    }
+    public void displayEmpWage() {
+        System.out.println("Total Wage =" + sum_totalWage);
+        System.out.println("Total Hours =" + total_hrs);
+    }
+    public static void main(String[] args) {
+        EmployeeWageBuilder employeeWageBuilder =new EmployeeWageBuilder();
+        employeeWageBuilder.empWageCalculation();
+        employeeWageBuilder.displayEmpWage();
     }
 }
